3f7b5316559d921845d968f4006f0517ff1454e3
Josh260519 <129236841+Josh260519@users.noreply.github.com>
2023-07-20 14:07:02 +0800

    Update client.py

--- skills/question3/client.py
+++ skills/question3/client.py
@@ -1,33 +1,38 @@
 import socket
 
-# Prompt the user for server IP address and port number
-server_ip = input("Enter the server IP address: ")
-server_port = int(input("Enter the server port number: "))
 
-# Creates a socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def conversion_input():
+    while True:
+        try:
+            pressure = float(input("Enter pressure in bar: "))
+            return pressure
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
-# Connect to the server
-server_address = (server_ip, server_port)
-client_socket.connect(server_address)
+def main():
+    # Prompt the user for server IP address and port number
+    server_ip = input("Enter the server IP address: ")
+    server_port = int(input("Enter the server port number: "))
 
-while True:
-    # Prompt the user for input
-    user_input = input("Enter a string (or press 'Q' to quit): ")
+    # Creates a socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Break the loop if 'Q' or 'q' is entered
-    # lower() to ignore case sensitivity
-    if user_input.lower() == 'q':
-        break
+    # Connect to the server
+    server_address = (server_ip, server_port)
+    client_socket.connect(server_address)
+    while True:
+        # Prompt the user for input
+        pressure = conversion_input()
+    
+        # Send the user input to the server
+        client_socket.send(str(pressure).encode())
 
-    # Send the user input to the server
-    client_socket.sendall(user_input.encode('utf-8'))
+        # Receive the atm pressure
+        atmosphere_pressure = client_socket.recv(1024).decode()
+        print(f"Pressure in atmosphere-standard: {atmosphere_pressure} atm")
+    
+    # Close the connection
+    client_socket.close()
 
-    # Receive the reply from the server
-    reply = client_socket.recv(1024).decode('utf-8')
-
-    # Print out the reply
-    print("Reply from server:", reply)
-
-# Close the connection
-client_socket.close()
+if __name__ == "__main__"
+main()
