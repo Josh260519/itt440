b77580882ac00807a57420d7eeb0c1316b71b384
Josh260519 <129236841+Josh260519@users.noreply.github.com>
2023-07-20 12:01:35 +0800

    Update clientprogram.py
    Client program to connect to itt440 skill assessment server

--- skills/clientprogram.py
+++ skills/clientprogram.py
@@ -1,16 +1,17 @@
 import socket
 
-# Prompt the user for server IP address and port number
+# Enter the server address instructed by lecturer and the port number
 server_ip = input("Enter the server IP address: ")
 server_port = int(input("Enter the server port number: "))
 
-# Creates a socket
+# To create a socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
+# To connect to the server
 server_address = (server_ip, server_port)
 client_socket.connect(server_address)
 
+# looping may not be necessary but to ensure the link is received, looping was implemented as a backup feature
 while True:
     # Prompt the user for input
     user_input = input("Enter a string (or press 'Q' to quit): ")
@@ -20,13 +21,14 @@ while True:
     if user_input.lower() == 'q':
         break
 
-    # Send the user input to the server
+    # Sending input to server
     client_socket.sendall(user_input.encode('utf-8'))
 
-    # Receive the reply from the server
+    # Receiving the user input from server
+    # It is required to be able to receive the skill assessment link
     reply = client_socket.recv(1024).decode('utf-8')
 
-    # Print out the reply
+    # To get our link for the skill assess test
     print("Reply from server:", reply)
 
 # Close the connection
