dedb1238cf2bf17bfa18947a32a80b3e1bc6cafa
Josh260519 <129236841+Josh260519@users.noreply.github.com>
2023-07-20 15:08:56 +0800

    Update client.py

--- skills/question4/client.py
+++ skills/question4/client.py
@@ -11,19 +11,6 @@ def main():
 
     # Connect to the server
     server_address = (server_ip, server_port)
-    client_socket.connect(server_address)import socket
-
-
-def main():
-    # Prompt the user for server IP address and port number
-    server_ip = input("Enter the server IP address: ")
-    server_port = int(input("Enter the server port number: "))
-
-    # Creates a socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect to the server
-    server_address = (server_ip, server_port)
     client_socket.connect(server_address)
 
     while True:
@@ -48,4 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
